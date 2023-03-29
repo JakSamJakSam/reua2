938cb5776b9639ca2aeaacad7e5fd5e4386c1713
JakSam <JakSam79@gmail.com>
2023-03-29 15:07:22 +0300

    Мелкие правки

--- reua2/send_message.py
+++ reua2/send_message.py
@@ -4,7 +4,10 @@ from django.core.mail import send_mail
 import requests
 
 def send_email_to_staffs(subject, message='', html_message=''):
-    recipients = list(get_user_model().objects.filter(is_staff=True, is_active=True ).exclude(email='').values_list('email'))
+    recipients = [
+        l[0] for
+        l in get_user_model().objects.filter(is_staff=True, is_active=True ).exclude(email='').values_list('email')
+    ]
     send_mail(subject, message, None, recipients, fail_silently=not settings.DEBUG, html_message=html_message)
 
 def send_to_telegram(message):
