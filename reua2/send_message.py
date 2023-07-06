091843e576e61175b2b0e4ad5af6bcb8953cc738
JakSam <JakSam79@gmail.com>
2023-07-06 15:59:30 +0300

    new message template

--- reua2/send_message.py
+++ reua2/send_message.py
@@ -3,12 +3,14 @@ from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 import requests
 
+
 def send_email_to_staffs(subject, message='', html_message=''):
     recipients = [
         l[0] for
-        l in get_user_model().objects.filter(is_staff=True, is_active=True ).exclude(email='').values_list('email')
+        l in get_user_model().objects.filter(is_staff=True, is_active=True).exclude(email='').values_list('email')
     ]
     send_mail(subject, message, None, recipients, fail_silently=not settings.DEBUG, html_message=html_message)
+
 
 def send_to_telegram(message):
     token = settings.TELEGRAM_TOKEN
