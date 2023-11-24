efb3e8fcc78724d627891bd54f1c2b69d414b1df
JakSam <JakSam79@gmail.com>
2023-11-24 16:08:21 +0200

    Изменения Богдана

--- reua/static/reua/js/single_project12.js
+++ reua/static/reua/js/single_project12.js
@@ -1,16 +1,26 @@
+function changeImage(imageSrc) {
+	const mainImage = document.querySelector('.single__wrapper .main-image img');
+	mainImage.src = imageSrc;
+	mainImage.alt = imageSrc;
+}
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
 	const thumbnailsList = document.querySelector('.thumbnails-list');
 	const thumbnails = thumbnailsList.querySelectorAll('img');
-	let currentIndex = 1; // Изменили начальное значение на 1
+	let currentIndex = 2;
 
 	function setActiveItem(newIndex) {
 		thumbnails.forEach((thumbnail, index) => {
-			thumbnail.classList.remove('active', 'left', 'right');
+			thumbnail.classList.remove('active', 'left', 'right', 'center');
 			if (index === newIndex) {
 				thumbnail.classList.add('active');
-			} else if (index === newIndex - 1 || (newIndex === 0 && index === thumbnails.length - 1)) {
+			} else if (index === (newIndex + 1) % thumbnails.length) {
+				thumbnail.classList.add('center');
+			} else if (index === (newIndex - 1 + thumbnails.length) % thumbnails.length) {
 				thumbnail.classList.add('left');
-			} else if (index === newIndex + 1 || (newIndex === thumbnails.length - 1 && index === 0)) {
+			} else if (index === (newIndex + 2) % thumbnails.length) {
 				thumbnail.classList.add('right');
 			}
 		});
@@ -22,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	thumbnails.forEach((thumbnail, index) => {
 		thumbnail.addEventListener('click', function () {
-			const newIndex = Array.from(thumbnails).indexOf(thumbnail);
+			const newIndex = parseInt(thumbnail.getAttribute('data-index'));
 
 			if (newIndex !== currentIndex) {
 				setActiveItem(newIndex);
@@ -31,6 +41,16 @@ document.addEventListener('DOMContentLoaded', function () {
 		});
 	});
 
-	// Установка активной миниатюры второй по порядку
-	setActiveItem(1);
+	document.getElementById('right').addEventListener('click', function () {
+		currentIndex = (currentIndex + 1) % thumbnails.length;
+		setActiveItem(currentIndex);
+	});
+
+	document.getElementById('left').addEventListener('click', function () {
+		currentIndex = (currentIndex - 1 + thumbnails.length) % thumbnails.length;
+		setActiveItem(currentIndex);
+	});
+
+	// Set active thumbnail initially
+	setActiveItem(currentIndex);
 });
