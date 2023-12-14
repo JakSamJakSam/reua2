756e30dd39d57916a2f015a0ba073582774dbbc6
JakSam <JakSam79@gmail.com>
2023-12-14 14:58:02 +0200

    !

---
+++ reua/static/reua/js/single_project19.js
@@ -0,0 +1,54 @@
+function changeImage(imageSrc) {
+	const mainImage = document.querySelector('.single__wrapper .main-image img');
+	mainImage.src = imageSrc;
+	mainImage.alt = imageSrc;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const thumbnailsList = document.querySelector('.thumbnails-list');
+  const thumbnails = thumbnailsList.querySelectorAll('img');
+  let currentIndex = 0; // Изменяем начальный индекс на 0
+
+  function setActiveItem(newIndex) {
+    thumbnails.forEach((thumbnail, index) => {
+      thumbnail.classList.remove('active', 'left', 'right', 'center');
+      if (index === newIndex) {
+        thumbnail.classList.add('active');
+      } else if (index === (newIndex + 1) % thumbnails.length) {
+        thumbnail.classList.add('left');
+      } else if (index === (newIndex + 2) % thumbnails.length) {
+        thumbnail.classList.add('center');
+      } else if ((newIndex === 0 && index === thumbnails.length - 1) || (index === (newIndex - 1 + thumbnails.length) % thumbnails.length)) {
+        thumbnail.classList.add('right');
+      }
+    });
+
+    const mainImage = document.querySelector('.main-image img');
+    mainImage.src = thumbnails[newIndex].src;
+    mainImage.alt = thumbnails[newIndex].alt;
+  }
+
+  thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', function () {
+      const newIndex = parseInt(thumbnail.getAttribute('data-index'));
+
+      if (newIndex !== currentIndex) {
+        setActiveItem(newIndex);
+        currentIndex = newIndex;
+      }
+    });
+  });
+
+  document.getElementById('right').addEventListener('click', function () {
+    currentIndex = (currentIndex + 1) % thumbnails.length;
+    setActiveItem(currentIndex);
+  });
+
+  document.getElementById('left').addEventListener('click', function () {
+    currentIndex = (currentIndex - 1 + thumbnails.length) % thumbnails.length;
+    setActiveItem(currentIndex);
+  });
+
+  // Set active thumbnail initially
+  setActiveItem(currentIndex);
+});
